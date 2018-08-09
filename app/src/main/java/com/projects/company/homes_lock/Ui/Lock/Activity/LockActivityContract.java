b9cfc5e3ddd235e7c0e22a919ffe9ac15dceb399
Morteza Hoseini <mortezahosseinee@gmail.com>
2018-08-09 18:54:59 +0430

    create class diagram

--- app/src/main/java/com/projects/company/homes_lock/Ui/Lock/Activity/LockActivityContract.java
+++ app/src/main/java/com/projects/company/homes_lock/Ui/Lock/Activity/LockActivityContract.java
@@ -1,10 +1,12 @@
 package com.projects.company.homes_lock.Ui.Lock.Activity;
 
+import com.projects.company.homes_lock.Base.BaseInterface;
+
 /**
  * This is Contract for {@link LockActivity} and {@link LockActivityPresenter}
  */
 
-public interface LockActivityContract {
+public interface LockActivityContract{
     interface mMvpView {
     }
 
