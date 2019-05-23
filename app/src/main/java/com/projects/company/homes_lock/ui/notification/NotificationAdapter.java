e8769262ecf15cde4a40ad7202eb07decf11b3e7
Morteza <milad7111@chmail.ir>
2019-05-23 14:04:15 +0330

    show all needed data from notification in list

--- app/src/main/java/com/projects/company/homes_lock/ui/notification/NotificationAdapter.java
+++ app/src/main/java/com/projects/company/homes_lock/ui/notification/NotificationAdapter.java
@@ -2,16 +2,18 @@ package com.projects.company.homes_lock.ui.notification;
 
 import android.app.Activity;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.projects.company.homes_lock.R;
 import com.projects.company.homes_lock.database.tables.Notification;
 
+import java.util.Date;
 import java.util.List;
 
 public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapter.NotificationAdapterViewHolder> {
@@ -40,6 +42,10 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
     public void onBindViewHolder(@NonNull NotificationAdapterViewHolder notificationsAdapterViewHolder, final int i) {
         if (mNotificationList != null) {
             notificationsAdapterViewHolder.txvNotificationTitle.setText(mNotificationList.get(i).getContentTitle());
+            notificationsAdapterViewHolder.txvNotificationSubtitle.setText(mNotificationList.get(i).getSummarySubText());
+            notificationsAdapterViewHolder.txvNotificationMessage.setText(mNotificationList.get(i).getMessage());
+            notificationsAdapterViewHolder.txvNotificationTime.setText(new Date(mNotificationList.get(i).getSentTime()).toString());
+
             notificationsAdapterViewHolder.itemView.setOnClickListener(v -> {
             });
         }
@@ -55,12 +61,22 @@ public class NotificationAdapter extends RecyclerView.Adapter<NotificationAdapte
 
     //region Declare Classes & Interfaces
     class NotificationAdapterViewHolder extends RecyclerView.ViewHolder {
+        ImageView imgNotificationPriority;
+        CheckBox imgNotificationStatus;
         TextView txvNotificationTitle;
+        TextView txvNotificationSubtitle;
+        TextView txvNotificationMessage;
+        TextView txvNotificationTime;
 
         private NotificationAdapterViewHolder(View itemView) {
             super(itemView);
 
+            imgNotificationPriority = itemView.findViewById(R.id.img_notification_priority);
+            imgNotificationStatus = itemView.findViewById(R.id.chb_notification_status);
             txvNotificationTitle = itemView.findViewById(R.id.txv_notification_title);
+            txvNotificationSubtitle = itemView.findViewById(R.id.txv_notification_subtitle);
+            txvNotificationMessage = itemView.findViewById(R.id.txv_notification_message);
+            txvNotificationTime = itemView.findViewById(R.id.txv_notification_time);
         }
     }
     //endregion Declare Classes & Interfaces
